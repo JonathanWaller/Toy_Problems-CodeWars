86005cc0976e0245e52d67e20bcf57033291c326
JonathanWaller <waller.j27@gmail.com>
2018-12-06 10:54:19 -0600

    2 problems

--- index.js
+++ index.js
@@ -7,4 +7,33 @@ function multiply(a, b) {
   return a * b;
 }
 
-2;
+// 2. Create a function (or write a script in Shell) that takes an integer as an argument and
+// returns "Even" for even numbers or "Odd" for odd numbers.
+
+// Solution:
+function even_or_odd(number) {
+  newNum = Math.abs(number);
+  if (newNum % 2 == 0) {
+    return "Even";
+  } else if (newNum % 2 == 1) {
+    return "Odd";
+  }
+}
+
+// 3. Given an array of integers your solution should find the smallest integer.
+
+Solution:
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      let myArr = 0;
+      myArr = args[0];
+      for(let i=1;i<args.length;i++){
+        if(args[i]<myArr){
+          myArr = args[i];
+        }
+      }
+      return myArr;
+    }
+  }
+
+  
