968263499a129f6f12a2cf6736368e2041815a80
JonathanWaller <waller.j27@gmail.com>
2018-12-27 12:05:39 -0600

    added new problem

--- index.js
+++ index.js
@@ -37,3 +37,13 @@ class SmallestIntegerFinder {
   }
 
   
+  // 4. Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+  Solution:
+  function boolToWord( bool ){
+    if (bool == true) {
+      return "Yes";
+      } else if (bool == false){
+      return "No";
+      }
+   }
